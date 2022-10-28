49f506e66b7f8b7f31d2f21e42085ad4aa76302e
tonyspunei <tony.spunei@yahoo.com>
2022-10-28 20:18:32 +0200

    find strongest correlation

--- script.js
+++ script.js
@@ -228,9 +228,21 @@ const JOURNAL = [
   },
 ];
 
+function phi(table) {
+  return (
+    (table[3] * table[0] - table[2] * table[1]) /
+    Math.sqrt(
+      (table[2] + table[3]) *
+        (table[0] + table[1]) *
+        (table[1] + table[3]) *
+        (table[0] + table[2])
+    )
+  );
+}
+
 function tableFor(event, journal) {
   let table = [0, 0, 0, 0];
-  for (let entry of JOURNAL) {
+  for (let entry of journal) {
     let index = 0;
     if (entry.events.includes(event)) index++;
     if (entry.squirrel) index += 2;
@@ -239,5 +251,32 @@ function tableFor(event, journal) {
   return table;
 }
 
-console.log(tableFor("pizza", JOURNAL));
-// [ 76, 9, 4, 1 ]
+function journalEvents(journal) {
+  let events = [];
+  for (let entry of journal) {
+    for (let event of entry.events) {
+      if (!events.includes(event)) {
+        events.push(event);
+      }
+    }
+  }
+  return events;
+}
+
+// for (let event of journalEvents(JOURNAL)) {
+//   const correlation = phi(tableFor(event, JOURNAL));
+//   if (correlation > 0.1 || correlation < -0.1) {
+//     console.log(`${event} : ${correlation}`);
+//   }
+// }
+
+for (let entry of JOURNAL) {
+  if (
+    entry.events.includes("peanuts") &&
+    !entry.events.includes("brushed teeth")
+  ) {
+    entry.events.push("peanut teeth");
+  }
+}
+
+console.log(phi(tableFor("peanut teeth", JOURNAL)));
